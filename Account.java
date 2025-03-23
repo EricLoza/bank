60ecaa16d890bd6827357aaab694c49bb1c0e698
Eric Loza <96986695+EricLoza@users.noreply.github.com>
2025-03-23 12:03:25 -0400

    First draft

--- Account.java
+++ Account.java
@@ -1,26 +1,49 @@
 public class Account {
     
     String name;
-    double balance, amount; 
-    int age; 
-    Account(String name, int age, double balance) {
+    double balance; 
+    final int pin, age;
+
+    Account(String name, final int age, final int pin, double balance) {
         this.name = name;
+        this.age = age;
+        this.pin = pin; 
         this.balance = balance;
-        this.age = age; 
     }
 
-    public void add() {
+    public Boolean login(String loginName, int loginPin) {
+        if(loginName.equals(name) && loginPin == pin) {
+
+        } else {
+            System.out.println("Incorrect credentials!");
+            return false; 
+        }
+        return true; 
 
     }
 
-    public void withdraw() {
+    public void depost(double amount) {
+        balance += amount; 
+        System.out.printf("$%.2f successfully added.", amount);
+        System.out.println();
+    }
 
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount; 
+            System.out.printf("$-%.2f successfully withdrawn.", amount);
+        } else {
+            System.out.print("Inusfficient Funds D:");
+        }
+        
+        System.out.println();
     }
 
     public void printInfo() {
-
-        System.out.println("Name" + name);
-        System.out.println("Age" + age);
-        System.out.println("Balance" + balance);
+        System.out.println("-----Account Information-----");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Balance: " + balance);
+        System.out.println();
     }
 }
--- Main.java
+++ Main.java
@@ -3,17 +3,54 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        double amount = 0; 
         Scanner scanner = new Scanner(System.in);
-        Account account = new Account("", 0, 0.0);
+        Account account = new Account("user", 45, 12345, 100.0);
 
-        System.out.println("Enter your name: ");
-        account.name = scanner.nextLine();
+        System.out.println("Enter your account name: ");
+        String loginName = scanner.nextLine();
 
-        System.out.println("Enter your age: ");
-        account.age = scanner.nextInt();
+        System.out.println("Enter your pin: ");
+        int loginPin = scanner.nextInt();
 
-        System.out.println("Enter your balance:");
-        account.balance = scanner.nextDouble();
+        System.out.println("\033c");
+
+        while(account.login(loginName, loginPin).booleanValue()) {
+            int choice; 
+            System.out.println("What would you like to do? ");
+            System.out.print("1 = depost, 2 = withdraw, 3 = account info, -1 = quit: ");
+            choice = scanner.nextInt(); 
+            if (choice == -1) {
+                System.out.println("Successful Logout");
+                break; 
+            } else {
+                switch (choice) {
+                    case 1:
+                        System.out.println("\033c");
+                        System.out.println("How much Would you like to deposit? ");
+                        amount = scanner.nextDouble(); 
+                        account.depost(amount);
+
+                        break; 
+                    case 2:
+                        System.out.println("\033c");
+                        System.out.println("How much Would you like to withdraw? ");
+                        amount = scanner.nextDouble(); 
+                        account.withdraw(amount);
+                        break;
+                    case 3:
+                        System.out.println("\033c");
+                        account.printInfo();
+                        break;
+                    default:
+                        System.out.println("\033c");
+                        System.out.println("Incorrect input!");
+                        break;
+                }
+            }
+
+        }
+
         scanner.close();
     }
 }
